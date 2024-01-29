16ecf83ae3590abcf44afb6edd6f7c194988ccb2
Muhammad Saad Anjum <l1s21bsse0112@ucp.edu.pk>
2024-01-29 11:53:42 +0500

    Initial Commit, Addition of HotelRoom Class Definition

---
+++ HotelRoom.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class HotelRoom{
+	private:
+		int roomNum;
+		int floorNum;
+		
+	public:
+		
+		HotelRoom();
+		void BookRoom();
+		void CheckoutRoom();
+		bool isRoomBooked();
+};
+
+int main (){
+	
+	return 0;
+}
